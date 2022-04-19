f02160218ece51600df2bd6023eb32a018d5bfb8
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-18 21:58:14 -0400

    Page layout done, on to the fetch hook in my next work session!

---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+
+
+// styles
+import './Banner.css'
+
+
+export default function Banner() {
+  return (
+    <div>
+      <div className="banner">
+        <div className="container">
+          <h1>Welcome to... <strong>The Final Frontier</strong>!</h1>
+          <p>Have a look, and see what might be waiting out there in Space for us to discover.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+
+// styles
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="footer">
+        <div className="footer-info">
+          Images on this website are provided through NASA's Image API.
+          For more information visit <a href="https://api.nasa.gov/">https://api.nasa.gov/</a>
+        </div>
+      </div>
+    </div>
+  )
+}
