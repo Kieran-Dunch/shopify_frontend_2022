ac5b4d9ae0662ffc96b3d7d790979fd8f5dbfbae
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-29 13:25:27 -0400

    finished user login/logout logic, moving onto pages now

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import './App.css';
 import Banner from "./components/Banner.js"
 import ImageList from "./components/ImageList.js";
 import Footer from './components/Footer.js'
-import { UserContext } from './context/UserContext';
 
 
 
 function App() {
-  // context
-  // const { authIsReady, user } = UserContext()
   return (
     <div className="App">
       <Banner/>
