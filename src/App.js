72a8b3ed8568b30ff6696e8851015fcc9f7e25b0
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-29 19:28:26 -0400

    added routes and react router dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 // pages
+import { Gallery } from "./pages/Gallery";
+import { Login } from "./pages/forms/Login";
+import { Signup } from "./pages/forms/Signup"
 
 // components
 import Banner from "./components/Banner.js"
@@ -15,6 +19,22 @@ function App() {
       <Banner/>
       <ImageList />
       <Footer />
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          {/* <Home/> */}
+        </Route>
+        <Route exact path="/Gallery">
+          <Gallery/>
+        </Route>
+        <Route path="/Login">
+          <Login/>
+        </Route>
+        <Route  path="/Signup">
+          <Signup/>
+        </Route>
+      </Switch>
+      </BrowserRouter>
     </div>
   );
 }
