fdd87356e8c250f28c627275eca105da7d867602
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-29 12:59:44 -0400

    wrote the login form and related hook

---
+++ src/hooks/UseSignup.js
@@ -0,0 +1,49 @@
+import { useState, useEffect } from "react";
+import { projectAuth } from "../firebase/config";
+import {useAuthContext} from './useAuthContext'
+
+
+export const useSignup = () => {
+  const [ isCancelled, setIsCancelled] = useState(false)
+  const [error, setError] = useState(null)
+  const [isPending, setIsPending] = useState(false)
+  const { dispatch } = useAuthContext()
+
+  const signup = async (email, password) => {
+    setError(null)
+    setIsPending(true)
+
+    try {
+      // signup user
+      const response = await projectAuth.createUserWithEmailAndPassword(email, password)
+
+      if (!response) {
+        throw new Error("Could not complete signup. Please try again.")
+      }
+
+      // dispatch login action
+      await dispatch({ type: 'LOGIN', payload: response.user })
+
+      // update state
+      if (!isCancelled) {
+        setIsPending(false)
+        setError(null)
+      }
+    } catch(error) {
+      if (!isCancelled) {
+        console.log(error.message)
+        setError(error.message)
+        setIsPending(false)
+      }
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      setIsCancelled(true)
+    }
+  }, [])
+
+
+  return { error, isPending, signup }
+}
