bacdd51a9266db677df0bcd9edafb1e5f280f5df
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-29 13:14:03 -0400

    testing userauth and context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 
+// pages
+
 // components
 import Banner from "./components/Banner.js"
 import ImageList from "./components/ImageList.js";
 import Footer from './components/Footer.js'
+import { UserContext } from './context/UserContext';
+
+
 
 function App() {
+  // context
+  const { authIsReady, user } = UserContext()
   return (
     <div className="App">
       <Banner/>
