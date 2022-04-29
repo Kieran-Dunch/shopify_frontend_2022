8b747a2139f1dd0082e85a1bac3a74ffb7c80052
Kieran Dunch <dunch.kieran@gmail.com>
2022-04-29 12:36:56 -0400

    finished config file

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+// database imports
+import firebase from 'firebase/app'
+// import authorisation
+import 'firebase/auth'
+// import liked photo & user storage
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiuGPdmh7yP5c4Y3L9BdjpDZqpbCSeYG8",
+  authDomain: "a-touch-of-space.firebaseapp.com",
+  projectId: "a-touch-of-space",
+  storageBucket: "a-touch-of-space.appspot.com",
+  messagingSenderId: "935189746838",
+  appId: "1:935189746838:web:c9768ebd72a15307185eb7"
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+export { projectFirestore, projectAuth }
